3b9a7027649ec22ed0d631292c8fbd3591f6692e
Bala-sakthi <balaabimanyugnc@gmail.com>
2024-01-30 17:54:43 +0530

    Api slice created

---
+++ src/redux/features/Store.js
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import UserReducer from "../features/userSlice";
+import { AuthApi } from "./api/AuthApi";
+import { TrainInfoApi } from "./api/TrainInfoApi";
+import { TrainApi } from "./api/TrainApi";
+import { StationApi } from "./api/StationApi";
+import { NotificationApi } from "./api/NotificationApi";
+
+export const store = configureStore({
+  reducer: {
+    User: UserReducer,
+    [AuthApi.reducerPath]: AuthApi.reducer,
+    [TrainInfoApi.reducerPath]:TrainInfoApi.reducer,
+    [TrainApi.reducerPath]:TrainApi.reducer,
+    [StationApi.reducerPath]:StationApi.reducer,
+    [NotificationApi.reducerPath]:NotificationApi.reducer,
+    },
+  devTools: process.env.NODE_ENV === "development",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([
+      AuthApi.middleware,TrainInfoApi.middleware,TrainApi.middleware,StationApi.middleware,NotificationApi.middleware,
+
+       ]),
+});
