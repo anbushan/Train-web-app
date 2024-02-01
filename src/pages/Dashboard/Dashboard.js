116386e007b92c08c6341dab9879803f6d1ec036
Bala-sakthi <balaabimanyugnc@gmail.com>
2024-02-01 19:37:23 +0530

    making some ui changes in Dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,139 +1,197 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import user from "../../assets/images/user.png"
+import Chart from "react-apexcharts";
+
 
 const Dashboard = () => {
+  const [state,setState]=useState( {
+    options: {
+      colors:["#db6300"],
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: [2023,2024,2025,2026,2027,2028,2029,2030]
+      }
+    },
+    
+    series: [
+      {
+        name: "series-1",
+        data: [0,6,12,18,24]
+      }
+    ]
+  });
   return (
-    <Container className="vh-100">
+    <Container className="">
       <Row className="mt-4 mx-2">
-        <Col className="d-flex align-items-start">
-          <h2>Welcome</h2>
+        <Col className="d-flex align-items-start shadow rounded p-3">
+          <h4>Dashboard</h4>
         </Col>
       </Row>
 
-      <Row className="d-flex flex-row justify-content-center align-items-center mt-3">
-        <Col xs={12} md={3} className="mx-4 my-2 shadow rounded p-4">
-          <h5 className="text-center no-wrap">User Count</h5>
-          <h3 className="text-center">25</h3>
-          <div>Logo</div>
-        </Col>
-
-        <Col xs={12} md={3} className="mx-4 my-2 shadow rounded p-4">
-          <h5 className="text-center no-wrap">Feedback Count</h5>
-          <h3 className="text-center">28</h3>
-          <div>Logo</div>
-        </Col>
-
-        <Col xs={12}md={3}  className="mx-4 my-2 shadow rounded p-4">
-          <h5 className="text-center no-wrap">Withdraw Request</h5>
-          <h3 className="text-center">30</h3>
-          <div>Logo</div>
-        </Col>
-      </Row>
-
-
-
-      <Row className=" mt-4 mx-4">
-      <Col className="shadow rounded my-2 m-3">
-        <h4 className="mt-3 mx-4">Recently Joined</h4>
-        <Row className="mt-4">
-          {/* Image column */}
-          <Col xs={4} className="mx-4">
-            <img  src={user}
-                width={50} alt="user" />
-          </Col>
-
-          {/* Text content column */}
-          <Col xs={6}>
-          <h4> <Row>John Deo</Row></h4>
-            <Row>Recently Joined</Row>
-          </Col>
-        </Row>
-        <hr/>
-
-        <Row className="mt-3"> 
-          {/* Image column */}
-          <Col xs={4} className="mx-4">
-            <img  src={user}
-                width={50} alt="user" />
-          </Col>
-
-          {/* Text content column */}
-          <Col xs={6}>
-          <h4> <Row>John Deo</Row></h4>
-            <Row>Recently Joined</Row>
-          </Col>
-        </Row>
-        <hr/>
-
-        <Row className="mt-4">
-          {/* Image column */}
-          <Col xs={4} className="mx-4">
-            <img  src={user}
-                width={50} alt="user" />
-          </Col>
-
-          {/* Text content column */}
-          <Col xs={6}>
-            <h4><Row>John Deo</Row></h4>
-            <Row>Recently Joined</Row>
-          </Col>
-        </Row>
-        <hr/>
-
-        <Row className="mt-3 mb-3">
-          {/* Image column */}
-          <Col xs={4} className="mx-4">
-            <img  src={user}
-                width={50}alt="user" />
-          </Col>
-
-          {/* Text content column */}
-          <Col xs={6}>
-          <h4> <Row>John Deo</Row></h4>
-            <Row>Recently Joined</Row>
-          </Col>
-        </Row>
-        <hr/>
-
-
-      </Col>
-
-      <Col className="shadow rounded my-2 m-3">
       
-      <h4 className="mt-3 mx-4">Withdraw Request</h4>
 
-      <Row className="mt-3 mb-3">
-          {/* Image column */}
-          <Col xs={4} className="mx-4">
-            <img  src={user}
-                width={50} alt="user" />
-          </Col>
+    <Row  className="mt-5">
 
-          {/* Text content column */}
-          <Col xs={6}>
-          <h4> <Row>John Deo</Row></h4>
-            <Row>Recently Joined</Row>
-          </Col>
-        </Row>
-        <hr/>
+<Col>
+<Row>
 
-        <Row className="mt-3 mb-3">
-          {/* Image column */}
-          <Col xs={4} className="mx-4">
-            <img  src={user}
-                width={50}alt="user" />
-          </Col>
 
-          {/* Text content column */}
-          <Col xs={6}>
-          <h4> <Row>John Deo</Row></h4>
-            <Row>Recently Joined</Row>
-          </Col>
-        </Row>
-        <hr/>
-        
-      </Col>
+
+  <Col className="d-flex flex-row  shadow rounded p-3 m-2">
+  
+  <Col className="d-flex flex-column"><img
+                src={user}
+                width={60}
+                className="rounded-circle p-2"
+                alt="..."
+              ></img></Col>
+  <Col className="text-end pt-2">
+  <Row className="fs-6 fw-bolder">Total</Row>
+  <Row  className="fs-14 fw-bolder">$ 23,548</Row>
+  <Row>347 bottles bottles</Row>
+  </Col>
+
+  </Col>
+
+
+ <Col className="d-flex flex-row shadow rounded p-3 m-2">
+  
+  <Col className="d-flex flex-column"><img
+                src={user}
+                width={60}
+                className="rounded-circle p-2"
+                alt="..."
+              ></img></Col>
+  <Col className="text-end pt-2">
+  <Row className="fs-6 fw-bolder" >Users Count</Row>
+  <Row className="fs-14 fw-bolder" >$ 23,548</Row>
+  <Row >347 bottles bottles</Row>
+  </Col>
+
+  </Col>
+
+
+
+</Row>
+<Row>
+
+<Col className="d-flex flex-row justify-content-between align-items-center text-center flex-wrap shadow rounded p-3 m-2">
+  
+  <Col className="d-flex flex-column  "><img
+                src={user}
+                width={60}
+                className="rounded-circle p-2"
+                alt="..."
+              ></img></Col>
+  <Col className="text-end pt-2">
+  <Row className="fs-6 fw-bolder">Feedback Count</Row>
+  <Row className="fs-14 fw-bolder">$ 23,548</Row>
+  <Row>347 bottles bottles</Row>
+  </Col>
+
+  </Col>
+
+  <Col className="d-flex flex-row shadow rounded p-3 m-2">
+  
+  <Col className="d-flex flex-column"><img
+                src={user}
+                width={60}
+                className="rounded-circle p-2"
+                alt="..."
+              ></img></Col>
+  <Col className="text-end pt-2">
+  <Row className="fs-6 fw-bolder">Withdraw Request</Row>
+  <Row  className="fs-14 fw-bolder">$ 23,548</Row>
+  <Row>347 bottles bottles</Row>
+  </Col>
+
+  </Col>
+</Row>
+
+
+</Col>
+
+
+
+
+
+
+
+
+
+  <Col xs={12} md={8} lg={6} xxl={6} className="d-flex flex-row justify-content-center align-items-center mt-2 mx-1 shadow rounded">
+    <Chart
+      options={state.options}
+      series={state.series}
+      type="area"
+      width="100%" // Use percentage for responsiveness
+    />
+  </Col>
+
+
+
+    </Row>
+
+    <Row className="d-flex flex-row justify-content-center align-items-center mb-3">
+<Col className="d-flex flex-column shadow rounded p-5 m-2">
+<Row className="mt-1 mx-4 fs-8 fw-bolder">Top Performing</Row>
+<Row className="">
+
+  <Col className="mt-3">1.international wines</Col>
+  <Col className="mt-3">139 orders</Col>
+
+</Row>
+
+<Row>
+
+  <Col className="mt-3">2.Santhosh_distributer</Col>
+  <Col className="mt-3">4 orders</Col>
+
+</Row>
+
+<Row>
+
+  <Col className="mt-3">3.Naveen ullah </Col>
+  <Col className="mt-3">10 orders</Col>
+
+</Row>
+
+</Col>
+
+
+
+
+<Col className="d-flex flex-column shadow rounded p-5 m-2">
+<Row className="mt-1 mx-4 fs-8 fw-bolder">Top Performing Retailers</Row>
+<Row className="">
+
+  <Col className="mt-3">1.Retailer Dev Env</Col>
+  <Col className="mt-3">44 orders</Col>
+
+</Row>
+
+<Row>
+
+  <Col className="mt-3">2.Shopify test site</Col>
+  <Col className="mt-3">23 orders</Col>
+
+</Row>
+
+<Row>
+
+  <Col className="mt-3">3.Ind Dev Wines</Col>
+  <Col className="mt-3">7 orders</Col>
+
+</Row>
+
+</Col>
+
+
+
+
     </Row>
       
     </Container>
