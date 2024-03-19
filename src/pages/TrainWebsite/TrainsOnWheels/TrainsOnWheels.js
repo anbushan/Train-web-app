6a3fad190793386425087d93f880cf0e0322eeb3
Bala-sakthi <balaabimanyugnc@gmail.com>
2024-03-19 21:08:09 +0530

    website pages added

---
+++ src/pages/TrainWebsite/TrainsOnWheels/TrainsOnWheels.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from '../Header'
+import Features from '../Features'
+import TravelInformation from '../TravelInformation'
+import PopularRoutes from '../PopularRoutes'
+import Explore from '../Explore'
+import Choose from '../Choose'
+import Downlaod from '../Download'
+import Footer from '../Footer'
+import HowItWork from '../HowItWork'
+
+const TrainsOnWheels = () => {
+  return (
+    <div>
+    <Header/>
+    <TravelInformation/>
+    <Features/>
+    <HowItWork/>
+    <PopularRoutes/>
+    <Explore/>
+    <Choose/>
+    <Downlaod/>
+    <Footer/>
+    </div>
+
+  )
+}
+
+export default TrainsOnWheels
