991750ad6dde9ed38ab96b35dbf41de4e8721569
Bala-sakthi <balaabimanyugnc@gmail.com>
2024-03-19 21:07:43 +0530

    website terms and condition added

---
+++ src/pages/TrainWebsite/TermsCondition.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+const TermsCondition = () => {
+  return (
+    <Container fluid>
+      <Row className="justify-content-center">
+        <Col xs={12} md={10} lg={8}>
+          <section>
+            <h2 className='text-center mt-4 mb-4'>Terms of Service</h2>
+            <h5>Guidelines for Reviews</h5>
+            <br />
+            <ul>
+              <li>you should have firsthand experience with the person/entity being reviewed</li>
+              <li>your reviews should not contain offensive profanity, or abusive, racist, offensive, or hate language</li>
+              <li>your reviews should not contain discriminatory references based on religion, race, gender, national origin, age, marital status, sexual orientation, or disability;</li>
+              <li>your reviews should not contain references to illegal activity;</li>
+              <li>you should not be affiliated with competitors if posting negative reviews;</li>
+              <li>you should not make any conclusions as to the legality of conduct;</li>
+              <li>you may not post any false or misleading statements;</li>
+              <li>you may not organize a campaign encouraging others to post reviews, whether positive or negative.</li>
+            </ul>
+            <p>We may accept, reject, or remove reviews in our sole discretion. We have absolutely no obligation to screen reviews or to delete reviews, even if anyone considers reviews objectionable or inaccurate. Reviews are not endorsed by us, and do not necessarily represent our opinions or the views of any of our affiliates or partners. We do not assume liability for any review or for any claims, liabilities, or losses resulting from any review. By posting a review, you hereby grant to us a perpetual, non-exclusive, worldwide, royalty-free, fully-paid, assignable, and sublicensable right and license to reproduce, modify, translate, transmit by any means, display, perform, and/or distribute all content relating to reviews.</p>
+           
+            <h5>Mobile Application License</h5>
+            <p><h6>Use License</h6></p>
+            <ul>
+              <li>decompile, reverse engineer, disassemble, attempt to derive the source code of, or decrypt the application;</li>
+              <li>make any modification, adaptation, improvement, enhancement, translation, or derivative work from the application;</li>
+              <li>violate any applicable laws, rules, or regulations in connection with your access or use of the application;</li>
+              <li>remove, alter, or obscure any proprietary notice (including any notice of copyright or trademark) posted by us or the licensors of the application;</li>
+              <li>use the application for any revenue generating endeavor, commercial enterprise, or other purpose for which it is not designed or intended;</li>
+              <li>make the application available over a network or other environment permitting access or use by multiple devices or users at the same time;</li>
+              <li>use the application for creating a product, service, or software that is, directly or indirectly, competitive with or in any way a substitute for the application;</li>
+              <li>use the application to send automated queries to any website or to send any unsolicited commercial e-mail;</li>
+              <li>use any proprietary information or any of our interfaces or our other intellectual property in the design, development, manufacture, licensing, or distribution of any applications, accessories, or devices for use with the application.</li>
+            </ul>
+
+            <p><h5>Apple and Android Devices</h5></p>
+            <ul>
+              <li>the license granted to you for our mobile application is limited to a non-transferable license to use the application on a device that utilizes the Apple iOS or Android operating systems, as applicable, and in accordance with the usage rules set forth in the applicable App Distributor’s terms of service;</li>
+              <li>we are responsible for providing any maintenance and support services with respect to the mobile application as specified in the terms and conditions of this mobile application license contained in these Terms of Use or as otherwise required under applicable law, and you acknowledge that each App Distributor has no obligation whatsoever to furnish any maintenance and support services with respect to the mobile application;</li>
+              <li>you acknowledge and agree that the App Distributors are third-party beneficiaries of the terms and conditions in this mobile application license contained in these Terms of Use, and that each App Distributor will have the right (and will be deemed to have accepted the right) to enforce the terms and conditions in this mobile application license contained in these Terms of Use against you as a third-party beneficiary thereof.</li>
+            </ul>
+
+            <h5>Social Media</h5>
+            <ul>
+              <li>providing your Third-Party Account login information through the Site; or</li>
+              <li>allowing us to access your Third-Party Account, as is permitted under the applicable terms and conditions that govern your use of each Third-Party Account.</li>
+            </ul>
+            <p>You represent and warrant that you are entitled to disclose your Third-Party Account login information to us and/or grant us access to your Third-Party Account, without breach by you of any of the terms and conditions that govern your use of the applicable Third-Party Account, and without obligating us to pay any fees or making us subject to any usage limitations imposed by the third-party service provider of the Third-Party Account. By granting us access to any Third-Party Accounts, you understand that:</p>
+            <ul>
+              <li>we may access, make available, and store (if applicable) any content that you have provided to and stored in your Third-Party Account (the “Social Network Content”) so that it is available on and through the Site via your account, including without limitation any friend lists; and</li>
+              <li>we may submit to and receive from your Third-Party Account additional information to the extent you are notified when you link your account with the Third-Party Account.</li>
+            </ul>
+
+            <p>Depending on the Third-Party Accounts you choose and subject to the privacy settings that you have set in such Third-Party Accounts, personally identifiable information that you post to your Third-Party Accounts may be available on and through your account on the Site. Please note that if a Third-Party Account or associated service becomes unavailable or our access to such Third-Party Account is terminated by the third-party service provider, then Social Network Content may no longer be available on and through the Site. You will have the ability to disable the connection between your account on the Site and your Third-Party Accounts at any time.</p>
+            <p>We will attempt to delete any information stored on our servers that was obtained through such Third-Party Accounts, except the username and profile picture that become associated with your account.</p>
+            <h5>Submissions</h5>
+            <p>You acknowledge and agree that any questions, comments, suggestions, ideas, feedback, or other information regarding the Site (“Submissions”) provided by you to us are non-confidential and shall become our sole property. We shall own exclusive rights, including all intellectual property rights, and shall be entitled to the unrestricted use and dissemination of these Submissions for any lawful purpose, commercial or otherwise, without acknowledgment or compensation to you. You hereby waive all moral rights to any such Submissions, and you hereby warrant that any such Submissions are original with you or that you have the right to submit such Submissions. You agree there shall be no recourse against us for any alleged or actual infringement or misappropriation of any proprietary right in your Submissions.</p>
+            <h5>Third-Party Websites and Content</h5>
+            <p>The Site may contain (or you may be sent via the Site) links to other websites (“Third-Party Websites”) as well as articles, photographs, text, graphics, pictures, designs, music, sound, video, information, applications, software, and other content or items belonging to or originating from third parties (“Third-Party Content”). Such Third-Party Websites and Third-Party Content are not investigated, monitored, or checked for accuracy, appropriateness, or completeness by us, and we are not responsible for any Third-Party Websites accessed through the Site or any Third-Party Content posted on, available through, or installed from the Site, including the content, accuracy, offensiveness, opinions, reliability, privacy practices, or other policies of or contained in the Third-Party Websites or the Third-Party Content.</p>
+            <p>Inclusion of, linking to, or permitting the use or installation of any Third-Party Websites or any Third-Party Content does not imply approval or endorsement thereof by us. If you decide to leave the Site and access the Third-Party Websites or to use or install any Third-Party Content, you do so at your own risk, and you should be aware these Terms of Use no longer govern. You should review the applicable terms and policies, including privacy and data gathering practices, of any website to which you navigate from the Site or relating to any applications you use or install from the Site. Any purchases you make through Third-Party Websites will be through other websites and from other companies, and we take no responsibility whatsoever in relation to such purchases which are exclusively between you and the applicable third party. You agree and acknowledge that we do not endorse the products or services offered on Third-Party Websites and you shall hold us harmless from any harm caused by your purchase of such products or services. Additionally, you shall hold us harmless from any losses sustained by you or harm caused to you relating to or resulting in any way from any Third-Party Content or any contact with Third-Party Websites.</p>
+            <h5>Intellectual Property Rights Infringements</h5>
+            <p>We respect the intellectual property rights of others. If you believe that any material available on or through the Site infringes upon any Intellectual Property Rights (“IPR”) you own or control, please immediately notify us at legal@Trains on Time.com. A copy of your notification will be sent to the person who posted or stored the material addressed in the notification. Please be advised that pursuant to applicable law you may be held liable for damages if you make material misrepresentations in your notification. Thus, if you are not sure that material located on or linked to by the Site infringes your IPR, you should consider first contacting an attorney.</p>
+          </section>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default TermsCondition;
